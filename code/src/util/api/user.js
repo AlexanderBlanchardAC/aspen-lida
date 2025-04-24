2c6a986432642e8b6549ed962758638e42208f98
Alexander <alexanderblanchardac@yahoo.com>
2025-04-24 22:06:43 +0100

    DIS-500: Add ability to share rewards to socials

--- code/src/util/api/user.js
+++ code/src/util/api/user.js
@@ -1227,7 +1227,8 @@ export async function fetchCampaigns(page = 1, pageSize = 20, filter = 'enrolled
 
      if (response.ok) {
           data = response.data;
-          console.log(data);
+          console.log("Campaigns returned:", data.result?.campaigns); // Logs the campaigns directly
+
           if (data.result?.page_current !== data.result?.page_total) {
                morePages = true;
           }
